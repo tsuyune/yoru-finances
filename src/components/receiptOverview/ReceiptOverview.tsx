bb4796be46fc25fd7cc1562052f84b6c84d746bb
Liudmila Fedorova <ritsumei@yandex.com>
2023-05-30 00:47:55 +0200

    feat: create footer of receipt overview

--- src/components/receiptOverview/ReceiptOverview.tsx
+++ src/components/receiptOverview/ReceiptOverview.tsx
@@ -1,10 +1,13 @@
+import ReceiptOverviewFooter from '../receiptOverviewFooter/ReceiptOverviewFooter';
 import ReceiptOverviewHeader from '../receiptOverviewHeader/ReceiptOverviewHeader';
 import styles from './ReceiptOverview.module.scss';
 
 export default function ReceiptOverview() {
+  const price = 233;
   return (
     <div className={styles['column']}>
       <ReceiptOverviewHeader />
+      <ReceiptOverviewFooter price={price} />
     </div>
   );
 }
---
+++ src/components/receiptOverviewFooter/ReceiptOverviewFooter.tsx
@@ -0,0 +1,19 @@
+import Typo from '../typo/Typo';
+import styles from './ReceiptOverviewfooter.module.scss';
+
+interface ReceiptProps {
+  price: number;
+}
+
+export default function ReceiptOverviewFooter({ price }: ReceiptProps) {
+  return (
+    <footer className={styles.footer}>
+      <Typo tag="span" type="secondary">
+        Total:
+      </Typo>
+      <Typo tag="span" type="price">
+        {price} $
+      </Typo>
+    </footer>
+  );
+}
